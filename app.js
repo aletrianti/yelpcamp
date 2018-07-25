0fd608e2a51903d7095effb7b1ca2b8068c8c877
Alessia Trianti <alestrianti@gmail.com>
2018-07-25 17:14:56 +0200

    Create campgrounds.ejs, add Bootstrap and move partials

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://images.pexels.com/photos/111362/pexels-photo-111362.jpeg?auto=compress&cs=tinysrgb&h=350"},
+        {name: "Granite Hill", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
+        {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/290448/pexels-photo-290448.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+    ];
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 // Start the server (http://localhost:3000) and create a callback function
 app.listen(3000, function() {
     console.log("The server has started!");
