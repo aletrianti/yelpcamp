06e1e598838e25222d4fd142ceb196d2abb2e3b3
Alessia Trianti <alestrianti@gmail.com>
2018-07-25 16:48:00 +0200

    Start server and create partials and views folders

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+// YelpCamp App
+// The Web Developer Bootcamp by Colt Steele
+
+var express = require("express");
+var app = express();
+
+// app.set - ejs
+app.set("view engine", "ejs");
+
+
+// app.get - pages
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+// Start the server (http://localhost:3000) and create a callback function
+app.listen(3000, function() {
+    console.log("The server has started!");
+});
