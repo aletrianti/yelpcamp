609daa8a9765f3a1b9de35c1437f170d181d0cbf
Alessia Trianti <alestrianti@gmail.com>
2018-07-26 17:42:03 +0200

    Rename campgrounds.ejs to index.ejs and create show.ejs

--- app.js
+++ app.js
@@ -12,16 +12,19 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true 
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
 /*
 // Create campground
 Campground.create({
     // Create a predefined campground
     name: "Sunset Valley", 
-    image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-camp-campfire-1061640.jpg&fm=jpg"
+    image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-camp-campfire-1061640.jpg&fm=jpg",
+    description: "If you like sunsets and valleys, this is the perfect camp for you!"
 }, function(error, campground) {
     if (error) {
         console.log("Oops, something went wrong!");
@@ -32,6 +35,7 @@ Campground.create({
     }
 });
 */
+
 
 // Use body-parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -53,7 +57,7 @@ app.get("/campgrounds", function(req, res) {
             console.log("Oops, something went wrong!");
             console.log(error);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -63,7 +67,8 @@ app.post("/campgrounds", function(req, res) {
     // Get info from the form in new.ejs and add the new camp to the array
     var name = req.body.name;
     var image = req.body.image;
-    var newCamp = {name: name, image: image}
+    var description = req.body.description;
+    var newCamp = {name: name, image: image, description: description}
 
     // Add a new campground to db
     Campground.create(newCamp, function(error, newlyCreated) {
@@ -80,6 +85,20 @@ app.post("/campgrounds", function(req, res) {
 // Render 'new campgrounds' page
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
+});
+
+// Show info about a campground
+app.get("/campgrounds/:id", function(req, res) {
+    // Find campground with its id
+    Campground.findById(req.params.id, function(error, foundCampground) {
+        if (error) {
+            console.log("Oops, something went wrong!");
+            console.log(error);
+        } else {
+            // Render show template with the campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 // Start the server (http://localhost:3000) and create a callback function
